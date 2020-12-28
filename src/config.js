0c508b7ff27d2459102252c613b2e56222faad07
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2020-12-28 11:07:40 -0600

    minor corrections

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export default 
+module.exports=
 {
-  API_ENDPOINT: 'https://localhost:3000/',
+  API_ENDPOINT: 'http://localhost:3000/',
   API_KEY: process.env.REACT_APP_API_KEY,
   API_DEV_ENDPOINT :'http://localhost:8000/',
 }
